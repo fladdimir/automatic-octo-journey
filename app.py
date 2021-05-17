08ec76fd1d42ed41109ae8541454926ae7dd7a06
fladdimir <fladdi.mir@gmx.de>
2021-05-17 23:26:22 +0200

    add html control

--- app.py
+++ app.py
@@ -20,18 +20,28 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello, World!"  #
+def index():
+    return app.send_static_file("index.html")
 
 
 @app.route("/volume", methods=["GET", "POST"])
-def get_volume() -> str:
+def volume() -> str:
     if request.method == "GET":
         return str(audio_controller_provider().get_level())
     elif request.method == "POST":
         new_value = float(request.args.get("new_value"))
         audio_controller_provider().set_level(new_value)
         return str(audio_controller_provider().get_level())
+
+
+@app.route("/change_volume", methods=["POST"])
+def change_volume() -> str:
+    change_by = float(request.args.get("by"))
+    current_volume = audio_controller_provider().get_level()
+    audio_controller_provider().set_level(current_volume + change_by)
+    current_volume = audio_controller_provider().get_level()
+    print(f"current volume: {current_volume}")
+    return str(current_volume)
 
 
 app.run("0.0.0.0", 5000, debug=True)
